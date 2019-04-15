6608b09c2083841a9d53f6047d2fc3b9bfd9fed5
Mislav Bašić <mislav.basic1@gmail.com>
2019-04-15 16:42:37 +0200

    css, fonts, img, js.

---
+++ src/main/webapp/js/main.js
@@ -0,0 +1,35 @@
+(function($) {
+	"use strict"
+
+	// Product Main img Slick
+	$('#product-main-img').slick({
+    infinite: true,
+    speed: 300,
+    dots: false,
+    arrows: true,
+    fade: true,
+    asNavFor: '#product-imgs',
+  });
+
+	// Product imgs Slick
+  $('#product-imgs').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    centerMode: true,
+    focusOnSelect: true,
+		centerPadding: 0,
+		vertical: true,
+    asNavFor: '#product-main-img',
+		responsive: [{
+        breakpoint: 991,
+        settings: {
+					vertical: false,
+					arrows: false,
+					dots: true,
+        }
+      },
+    ]
+  });
+
+})(jQuery);
